ae5115a794ff4557fdf0e6aca09b55648b371d03
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-06-03 16:20:09 -0300

    commit inicial

---
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/CatalogoProdutosApiApplication.java
@@ -0,0 +1,13 @@
+package com.meusprojetos.catalogo.produtos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutosApiApplication.class, args);
+	}
+
+}
