00f0bcf05256fe43ccab2ce4ddaf2d5da6135b98
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-06-09 16:59:13 -0300

    feat: criar entidade de usuario

---
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/entity/User.java
@@ -0,0 +1,24 @@
+package com.meusprojetos.catalogo.produtos.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_usuarios")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
