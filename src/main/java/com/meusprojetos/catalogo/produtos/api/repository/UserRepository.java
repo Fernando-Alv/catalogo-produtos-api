76e0dec9f5958a84b2dec6c8c50d8c7bc9fef4f8
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-06-11 19:33:11 -0300

    feat: criar UserRepository

---
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.meusprojetos.catalogo.produtos.api.repository;
+
+import com.meusprojetos.catalogo.produtos.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
