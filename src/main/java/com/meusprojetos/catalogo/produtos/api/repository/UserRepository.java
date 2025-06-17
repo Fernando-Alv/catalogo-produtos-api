1d61f02edc3f40434a76a829c7d25bc1a29f4279
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-06-17 16:09:08 -0300

    feat: adicionar findByEmail ao UserRepository para autenticação

--- src/main/java/com/meusprojetos/catalogo/produtos/api/repository/UserRepository.java
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.meusprojetos.catalogo.produtos.api.repository;
 
 import com.meusprojetos.catalogo.produtos.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String email);
 }
