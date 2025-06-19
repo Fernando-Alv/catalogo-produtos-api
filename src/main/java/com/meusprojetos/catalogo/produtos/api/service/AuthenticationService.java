53c4eb82839fd05cf56fcbca2758f54973b5118c
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-06-19 18:11:30 -0300

    feat: criar serviço de autenticação com UserDetailsService

---
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.meusprojetos.catalogo.produtos.api.service;
+
+import com.meusprojetos.catalogo.produtos.api.dto.UserRequestDTO;
+import com.meusprojetos.catalogo.produtos.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
