f20b92c336e95c677fcabfcc48af2943f62da622
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-06-23 19:54:31 -0300

    refactor: separar interface AuthenticationService da implementação AuthenticationServiceImpl

--- src/main/java/com/meusprojetos/catalogo/produtos/api/service/AuthenticationServiceImpl.java
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/service/AuthenticationServiceImpl.java
@@ -1,6 +1,6 @@
 package com.meusprojetos.catalogo.produtos.api.service;
 
-import com.meusprojetos.catalogo.produtos.api.dto.UserRequestDTO;
+import com.meusprojetos.catalogo.produtos.api.dto.AuthDTO;
 import com.meusprojetos.catalogo.produtos.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,12 +10,17 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class AuthenticationService implements UserDetailsService {
+public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public String getToken(AuthDTO authDTO) {
+        return "";
     }
 }
