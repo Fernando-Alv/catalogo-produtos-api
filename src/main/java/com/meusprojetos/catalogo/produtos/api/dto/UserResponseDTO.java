853dd696ebcda3520ae87b35da4d4485f534ba77
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-07-07 12:50:04 -0300

    refactor: adicionar id, name e role no UserResponseDTO

--- src/main/java/com/meusprojetos/catalogo/produtos/api/dto/UserResponseDTO.java
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/dto/UserResponseDTO.java
@@ -1,6 +1,10 @@
 package com.meusprojetos.catalogo.produtos.api.dto;
 
+import com.meusprojetos.catalogo.produtos.api.enums.UserRole;
+
 public record UserResponseDTO(
-        String username,
-        String email
+        Long id,
+        String name,
+        String email,
+        UserRole role
 ) { }
