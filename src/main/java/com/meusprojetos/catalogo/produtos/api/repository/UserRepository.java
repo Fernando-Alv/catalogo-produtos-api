e2303bbe1d7737aced457df35a1f70deaf4400f8
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-07-07 12:48:01 -0300

    refactor: usar User em findByEmail no UserRepository

--- src/main/java/com/meusprojetos/catalogo/produtos/api/repository/UserRepository.java
+++ src/main/java/com/meusprojetos/catalogo/produtos/api/repository/UserRepository.java
@@ -2,9 +2,8 @@ package com.meusprojetos.catalogo.produtos.api.repository;
 
 import com.meusprojetos.catalogo.produtos.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    UserDetails findByEmail(String email);
+    User findByEmail(String email);
 }
